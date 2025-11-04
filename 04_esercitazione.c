0ca0cd8f498d13c16d3b437611b9a56206533663
MarcoSac1 <marcosaccone8@gmail.com>
2025-11-04 16:02:41 +0100

    completato il 4 esercizio

--- 04_esercitazione.c
+++ 04_esercitazione.c
@@ -1,7 +1,28 @@
 //Conta dei numeri pari e dispari. Dato un array di interi, contare quanti sono pari e quanti sono dispari
-#include <stdio.h>
+#define DIM 10
 
 int main() {
+    int array[DIM];
+    int i;
+    int length = sizeof(array)/sizeof(array[0]);
+    int pari = 0;
+    int dispari = 0;
+
+    for (i = 0; i < length; i++) {
+        printf("inserisci il %d numero intero per l'array: \n", i+1);
+        scanf("%d", &array[i]);
+    }
+
+    for (i = 0; i < length; i++) {
+        if (array[i] % 2 == 0) {
+            pari++;
+        } else {
+            dispari++;
+        }
+    }
+
+    printf("Numeri pari: %d\n", pari);
+    printf("Numeri dispari: %d\n", dispari);
     
     return 0;
 }
